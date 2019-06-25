3e0117379326183b2d4eaf67eb776cd7100794d9
Dave Bushnell <dybushnell@gmail.com>
2019-06-25 18:07:17 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")    
   end
 
   def exclamation?
-
+    self.end_with?("!")    
   end
 
   def count_sentences
+    #regex -> .split(/.*) -> zero or more periods
+    punctuation_array = self.split(/[!.?]/)
+    punctuation_array.delete("")
+    #binding.pry
 
+    return punctuation_array.length
   end
 end
